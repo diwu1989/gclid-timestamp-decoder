917687b5ef7bc6fd4ac3fdc9bdb551b1a8b6ecc7
Alexey Kuntsevich <alexey.kuntsevich@gmail.com>
2019-07-23 17:39:30 +0200

    Adapt for python3 and long format of gclid

--- gclid_timestamp_decoder/decode.py
+++ gclid_timestamp_decoder/decode.py
@@ -1,15 +1,27 @@
 import base64
 import datetime
-import gclid_pb2
+from . import gclid_pb2
 
-def decode(gclid):
+def decode_v1(gclid):
     decoded_gclid = base64.urlsafe_b64decode(gclid + '=' * (4 - len(gclid) % 4))
-    g = gclid_pb2.Gclid()
+    g = gclid_pb2.GClIdv1()
     g.ParseFromString(decoded_gclid)
     return g
 
-def convert_to_dt(g):
-    return datetime.datetime.fromtimestamp(g.timestamp / 1000000).strftime('%Y-%m-%d %H:%M:%S')
+def convert_to_dt_v1(g):
+    return datetime.datetime.fromtimestamp(g.n1.n1.n1.val1 / 1000000)
+
+def decode_v2(gclid):
+    decoded_gclid = base64.urlsafe_b64decode(gclid + '=' * (4 - len(gclid) % 4))
+    g = gclid_pb2.GClIdv2()
+    g.ParseFromString(decoded_gclid)
+    return g
+
+def convert_to_dt_v2(g):
+    return datetime.datetime.fromtimestamp(g.n1.n2.val1)
 
 def get_timestamp_from_gclid(gclid):
-    return convert_to_dt(decode(gclid))
+    if len(gclid) > 60:
+        return convert_to_dt_v2(decode_v2(gclid))
+    else:
+        return convert_to_dt_v1(decode_v1(gclid))
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
 setup(name='gclid-timestamp-decoder',
-      version='0.1.2',
+      version='0.1.3',
       description='Decoding timestamp from Google gclid using Protocol Buffers.',
       author='Kenny Shen',
       author_email='kenny@machinesung.com',
       license='BSD',
-      url='https://github.com/qoelet/gclid-timestamp-decoder',
-      download_url='https://pypi.python.org/pypi/gclid-timestamp-decoder',
-      install_requires=['protobuf==2.6'],
+      url='https://github.com/jezzarax/gclid-timestamp-decoder',
+      install_requires=['protobuf>=3.0'],
       packages=['gclid_timestamp_decoder']
     )
